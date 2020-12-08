824fc82986d5c726c2f7aa7551fd9696643a6cfd
Alex Griffiths <alex-griffiths@outlook.com>
2020-12-09 09:58:19 +1100

    Complete Task 4.1

--- Mod4_1/app/src/main/java/com/example/mod4_1/FirstFragment.java
+++ Mod4_1/app/src/main/java/com/example/mod4_1/FirstFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -19,51 +20,44 @@ import java.util.List;
 public class FirstFragment extends Fragment {
 
     private Spinner pizza_list;
-
+    private ListView list;
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-
-
         View view = inflater.inflate(R.layout.fragment_first, container, false);
 
-        pizza_list = (Spinner)view.findViewById(R.id.pizzaList);
-
-        // Create array with pizza choices.
+        // Create array of pizza choices
         List<String> pizza_choices = new ArrayList<String>();
         pizza_choices.add("Ham and Pineapple");
         pizza_choices.add("Supreme");
         pizza_choices.add("Seafood");
         pizza_choices.add("Meatlovers");
 
+        // Get the list element
+        list = (ListView)view.findViewById(R.id.listView);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, pizza_choices);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        pizza_list.setAdapter(adapter);
-        pizza_list.setSelected(false);
+        // Apply an addapter to the list with the pizza choices array as content
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), R.layout.pizza_list, R.id.label, pizza_choices);
+        list.setAdapter(adapter);
 
-        pizza_list.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        // Listen for click on the list view.
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getActivity(), String.format("OnSelectedListener:\r\nPizza List: %s", String.valueOf(pizza_list.getSelectedItem())), Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = (String)list.getAdapter().getItem(position);
+                Toast.makeText(getActivity(), String.format("%s selected", item), Toast.LENGTH_LONG).show();
             }
         });
+
         return view;
     }
 
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-
     }
 
 }
